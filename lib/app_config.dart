4f3a16183ba8ef1e4470df488a3048c092522f58
juwelzan <juwelzan@gmail.com>
2026-03-14 03:02:44 +0800

    youtube lite commit 1

---
+++ lib/app_config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppConfig extends StatefulWidget {
+  const AppConfig({super.key});
+
+  @override
+  State<AppConfig> createState() => _AppConfigState();
+}
+
+class _AppConfigState extends State<AppConfig> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/feature/home/ui/youtube_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YoutubeHome extends StatefulWidget {
+  const YoutubeHome({super.key});
+
+  @override
+  State<YoutubeHome> createState() => _YoutubeHomeState();
+}
+
+class _YoutubeHomeState extends State<YoutubeHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
